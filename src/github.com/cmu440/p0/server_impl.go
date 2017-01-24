b8ef749028c067a2de1ee8102493234ecf06b147
Mohit Reddy <mohitreddy1996@gmail.com>
2017-01-25 03:30:46 +0530

    Coding done. Testing to be done

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -2,28 +2,143 @@
 
 package p0
 
+import (
+	"net"
+	"fmt"
+	"bufio"
+)
+
+type echoClient struct {
+	connection net.Conn
+	channel chan string
+}
+
 type multiEchoServer struct {
 	// TODO: implement this!
+	host string
+	echoClientChannel chan map[int] *echoClient
+	listener net.Listener
+	stopChannel chan bool
 }
 
 // New creates and returns (but does not start) a new MultiEchoServer.
 func New() MultiEchoServer {
 	// TODO: implement this!
-	return nil
+	server := &multiEchoServer{
+		host: "localhost",
+		echoClientChannel: make(chan map[int] *echoClient, 1),
+		stopChannel: make(chan bool),
+	}
+	return MultiEchoServer(server)
 }
 
 func (mes *multiEchoServer) Start(port int) error {
 	// TODO: implement this!
+	listener, err := net.Listen("tcp", fmt.Sprint(":%d", port))
+	mes.listener = listener
+	if err != nil{
+		fmt.Println("Error on listen : ", err)
+		return err
+	}
+	fmt.Printf("Server running on %s : %d", mes.host, port)
+
+	// serve or accept connection. (MultiEchoServer should)
+	go mes.serve()
 	return nil
 }
 
 func (mes *multiEchoServer) Close() {
 	// TODO: implement this!
+	for _, client := range <-mes.echoClientChannel{
+		client.connection.Close()
+	}
+	mes.listener.Close()
+	close(mes.stopChannel)
 }
 
 func (mes *multiEchoServer) Count() int {
 	// TODO: implement this!
-	return -1
+	clients := <-mes.echoClientChannel
+	count := len(clients)
+	mes.echoClientChannel <- clients
+	return count
 }
 
-// TODO: add additional methods/functions below!
+func (mes *multiEchoServer) serve()  {
+	messageChannel := make(chan string)
+	go broadcastMessage(messageChannel, mes.echoClientChannel)
+
+	clients := make(map[int]*echoClient)
+	mes.echoClientChannel <- clients
+	i:= 0
+	for{
+		connection, err := mes.listener.Accept()
+		if err!=nil{
+			select {
+			case <- mes.stopChannel:
+				return
+			default:
+			}
+			fmt.Println("Error on Accept : ", err)
+			continue
+		}
+		go handleConnection(i, connection, mes.echoClientChannel, messageChannel)
+		i++
+	}
+}
+
+func handleConnection(clientNumber int, connection net.Conn, echoClientChannel chan map[int] *echoClient, messageChannel chan <-string)  {
+	fmt.Printf("Client : %d : %v <-> %v \n", clientNumber, connection.LocalAddr(), connection.RemoteAddr())
+
+	EchoClientPtr := &echoClient{
+		connection: connection,
+		channel: make(chan string, 100),
+	}
+
+	clients := <- echoClientChannel
+	clients[clientNumber] = EchoClientPtr
+	echoClientChannel <- clients
+
+	go echo(EchoClientPtr)
+	defer EchoClientPtr.connection.Close()
+
+	rb := bufio.NewReader(connection)
+	for{
+		message, e := rb.ReadString('\n')
+		if e!=nil{
+			break
+		}
+		messageChannel <- message
+	}
+
+	clients <- echoClientChannel
+	delete(clients, clientNumber)
+	echoClientChannel <- clients
+	fmt.Printf("%d : Closed \n", clientNumber)
+}
+
+
+func echo(client *echoClient){
+	for{
+		message := <-client.channel
+		_, err := client.connection.Write([]byte(message))
+		if err!=nil{
+			break
+		}
+	}
+}
+
+func broadcastMessage(messageChannel <-chan string, echoClientChannel chan map[int] *echoClient)  {
+	for{
+		message := <-messageChannel
+		clients := <-echoClientChannel
+		for _, echoClient := range clients{
+			select {
+			case echoClient.channel <- message:
+			default:
+			}
+		}
+		echoClientChannel <- clients
+	}
+}
+
